bceac7e8f501db5f27c0fb2a2272805254fe83db
Vitaly Simakov <v.simakov@dcrt.it>
2020-07-05 17:51:07 +0500

    add: Readme and about project

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :redmine_warehouse do
   author 'VItaly Simakov'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/cocoarocket/Redmine-Plugins'
+  author_url 'https://github.com/cocoarocket'
 end
