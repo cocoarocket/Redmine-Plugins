26dc5f1a674032c3ac5eeae35c58658ec990e960
Vitaly Simakov <v.simakov@dcrt.it>
2020-07-08 06:19:47 +0500

    Ready

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! product, :id, :side, :created_at, :updated_at
+json.extract! product, :id, :name, :price, :number_count, :issue_id, :production_date, :created_at, :updated_at
 json.url product_url(product, format: :json)
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,8 +1,32 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class ProductsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def setup
+    3.times do |n|
+      name = (n + 1).to_s
+      price = Random.rand(10.0..500.0).truncate(2)
+      number_count = Random.rand(10..500)
+      Product.create!(name: "product_#{name}", price: price, number_count: number_count, production_date: Date.current, issue_id: 1)
+    end
+  end
+
+  def test_index_success
+    get :index
+    assert_response :success
+
+    assert_select 'tbody' do |elements|
+      elements.each do |element|
+        assert_select element, "tr", 3
+      end
+    end
+
+  end
+
+  def test_show_success
+    get :show, :params => {
+      :id => Product.last.id
+    }
+    assert_response :success
+    assert_select 'h3', Product.last.name
   end
 end
